0ab5c86f7de0608f94b6e1dbf148f81aa72a3873
Filip Chromý <f_chromy@utb.cz>
2022-01-16 02:30:31 +0100

    Add project files.

---
+++ GameControl.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace TicTacToe
+{
+    public static class GameControl
+    {
+        private static string _playerTurn = "X";
+        public static void PlaceMove(TextBlock textBlock)
+        {
+            textBlock.Text = _playerTurn;
+            ChangePlayer();
+        }
+        public static void ChangePlayer(string player)
+        {
+            // force select player
+            _playerTurn = player;
+        }
+        public static void ChangePlayer()
+        {
+            if (_playerTurn == "X")
+            {
+                _playerTurn = "O";
+            }
+            else
+            {
+                _playerTurn = "X";
+            }
+        }
+        public static void GameStateUpdate(TextBlock gameState)
+        {
+            if (gameState.Text.StartsWith("X"))
+            {
+                gameState.Text = "O plays";
+            }
+            else
+            {
+                gameState.Text = "X plays";
+            }
+        }
+        public static void ValidateWinnerX(
+            TextBlock button1, TextBlock button2, TextBlock button3, 
+            TextBlock button4, TextBlock button5, TextBlock button6,
+            TextBlock button7, TextBlock button8, TextBlock button9,
+            TextBlock gameState, List<Button> buttons
+            )
+        {
+            if (button1.Text == "X" && button2.Text == "X" && button3.Text == "X")
+            {
+                gameState.Text = "Player X Wins!";
+                foreach (Button bt in buttons)
+                {
+                    bt.IsEnabled = false;
+                }
+            }
+            else if (button4.Text == "X" && button5.Text == "X" && button6.Text == "X")
+            {
+                gameState.Text = "Player X Wins!";
+                foreach (Button bt in buttons)
+                {
+                    bt.IsEnabled = false;
+                }
+            }
+            else if (button7.Text == "X" && button8.Text == "X" && button9.Text == "X")
+            {
+                gameState.Text = "Player X Wins!";
+                foreach (Button bt in buttons)
+                {
+                    bt.IsEnabled = false;
+                }
+            }
+            else if (button1.Text == "X" && button4.Text == "X" && button7.Text == "X")
+            {
+                gameState.Text = "Player X Wins!";
+                foreach (Button bt in buttons)
+                {
+                    bt.IsEnabled = false;
+                }
+            }
+            else if (button2.Text == "X" && button5.Text == "X" && button8.Text == "X")
+            {
+                gameState.Text = "Player X Wins!";
+                foreach (Button bt in buttons)
+                {
+                    bt.IsEnabled = false;
+                }
+            }
+            else if (button3.Text == "X" && button6.Text == "X" && button9.Text == "X")
+            {
+                gameState.Text = "Player X Wins!";
+                foreach (Button bt in buttons)
+                {
+                    bt.IsEnabled = false;
+                }
+            }
+            else if (button1.Text == "X" && button5.Text == "X" && button9.Text == "X")
+            {
+                gameState.Text = "Player X Wins!";
+                foreach (Button bt in buttons)
+                {
+                    bt.IsEnabled = false;
+                }
+            }
+            else if (button3.Text == "X" && button5.Text == "X" && button7.Text == "X")
+            {
+                gameState.Text = "Player X Wins!";
+                foreach (Button bt in buttons)
+                {
+                    bt.IsEnabled = false;
+                }
+            }
+        }
+
+        public static void ValidateWinnerO(
+            TextBlock button1, TextBlock button2, TextBlock button3,
+            TextBlock button4, TextBlock button5, TextBlock button6,
+            TextBlock button7, TextBlock button8, TextBlock button9,
+            TextBlock gameState, List<Button> buttons
+            )
+        {
+            if (button1.Text == "O" && button2.Text == "O" && button3.Text == "O")
+            {
+                gameState.Text = "Player O Wins!";
+                foreach (Button bt in buttons)
+                {
+                    bt.IsEnabled = false;
+                }
+            }
+            else if (button4.Text == "O" && button5.Text == "O" && button6.Text == "O")
+            {
+                gameState.Text = "Player O Wins!";
+                foreach (Button bt in buttons)
+                {
+                    bt.IsEnabled = false;
+                }
+            }
+            else if (button7.Text == "O" && button8.Text == "O" && button9.Text == "O")
+            {
+                gameState.Text = "Player O Wins!";
+                foreach (Button bt in buttons)
+                {
+                    bt.IsEnabled = false;
+                }
+            }
+            else if (button1.Text == "O" && button4.Text == "O" && button7.Text == "O")
+            {
+                gameState.Text = "Player O Wins!";
+                foreach (Button bt in buttons)
+                {
+                    bt.IsEnabled = false;
+                }
+            }
+            else if (button2.Text == "O" && button5.Text == "O" && button8.Text == "O")
+            {
+                gameState.Text = "Player O Wins!";
+                foreach (Button bt in buttons)
+                {
+                    bt.IsEnabled = false;
+                }
+            }
+            else if (button3.Text == "O" && button6.Text == "O" && button9.Text == "O")
+            {
+                gameState.Text = "Player O Wins!";
+                foreach (Button bt in buttons)
+                {
+                    bt.IsEnabled = false;
+                }
+            }
+            else if (button1.Text == "O" && button5.Text == "O" && button9.Text == "O")
+            {
+                gameState.Text = "Player O Wins!";
+                foreach (Button bt in buttons)
+                {
+                    bt.IsEnabled = false;
+                }
+            }
+            else if (button3.Text == "O" && button5.Text == "O" && button7.Text == "O")
+            {
+                gameState.Text = "Player O Wins!";
+                foreach (Button bt in buttons)
+                {
+                    bt.IsEnabled = false;
+                }
+            }
+        }
+        public static void CheckForTie(List<TextBlock> buttonValues, TextBlock gameState, List<Button> bts)
+        {
+            bool allPositionsOccupied = true;
+            foreach (TextBlock tb in buttonValues)
+            {
+                if (tb.Text.Equals(""))
+                {
+                    allPositionsOccupied = false;
+                }
+            }
+            if (allPositionsOccupied && !gameState.Text.EndsWith("Wins!"))
+            {
+                foreach (Button bt in bts)
+                {
+                    bt.IsEnabled = false;
+                }
+                gameState.Text = "The game was a tie!";
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TicTacToe
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            GameControl.PlaceMove(buttonOneText);
+            GameControl.GameStateUpdate(gameState);
+            new List<Button>() { bt1, bt2, bt3, bt4, bt5, bt6, bt7, bt8, bt9 };
+            GameControl.ValidateWinnerX(
+                buttonOneText, buttonTwoText, buttonThreeText,
+                buttonFourText, buttonFiveText, buttonSixText,
+                buttonSevenText, buttonEightText, buttonNineText, gameState, new List<Button>() { bt1, bt2, bt3, bt4, bt5, bt6, bt7, bt8, bt9 });
+            GameControl.ValidateWinnerO(
+                buttonOneText, buttonTwoText, buttonThreeText,
+                buttonFourText, buttonFiveText, buttonSixText,
+                buttonSevenText, buttonEightText, buttonNineText, gameState, new List<Button>() { bt1, bt2, bt3, bt4, bt5, bt6, bt7, bt8, bt9 });
+            GameControl.CheckForTie(AllButtonValues(), gameState, AllButtons());
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            GameControl.PlaceMove(buttonTwoText);
+            GameControl.GameStateUpdate(gameState);
+            GameControl.ValidateWinnerX(
+                buttonOneText, buttonTwoText, buttonThreeText,
+                buttonFourText, buttonFiveText, buttonSixText,
+                buttonSevenText, buttonEightText, buttonNineText, gameState, new List<Button>() { bt1, bt2, bt3, bt4, bt5, bt6, bt7, bt8, bt9 });
+            GameControl.ValidateWinnerO(
+                buttonOneText, buttonTwoText, buttonThreeText,
+                buttonFourText, buttonFiveText, buttonSixText,
+                buttonSevenText, buttonEightText, buttonNineText, gameState, new List<Button>() { bt1, bt2, bt3, bt4, bt5, bt6, bt7, bt8, bt9 });
+            GameControl.CheckForTie(AllButtonValues(), gameState, AllButtons());
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            GameControl.PlaceMove(buttonThreeText);
+            GameControl.GameStateUpdate(gameState);
+            GameControl.ValidateWinnerX(
+                buttonOneText, buttonTwoText, buttonThreeText,
+                buttonFourText, buttonFiveText, buttonSixText,
+                buttonSevenText, buttonEightText, buttonNineText, gameState, new List<Button>() { bt1, bt2, bt3, bt4, bt5, bt6, bt7, bt8, bt9 });
+            GameControl.ValidateWinnerO(
+                buttonOneText, buttonTwoText, buttonThreeText,
+                buttonFourText, buttonFiveText, buttonSixText,
+                buttonSevenText, buttonEightText, buttonNineText, gameState, new List<Button>() { bt1, bt2, bt3, bt4, bt5, bt6, bt7, bt8, bt9 });
+            GameControl.CheckForTie(AllButtonValues(), gameState, AllButtons());
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            GameControl.PlaceMove(buttonFourText);
+            GameControl.GameStateUpdate(gameState);
+            GameControl.ValidateWinnerX(
+                buttonOneText, buttonTwoText, buttonThreeText,
+                buttonFourText, buttonFiveText, buttonSixText,
+                buttonSevenText, buttonEightText, buttonNineText, gameState, new List<Button>() { bt1, bt2, bt3, bt4, bt5, bt6, bt7, bt8, bt9 });
+            GameControl.ValidateWinnerO(
+                buttonOneText, buttonTwoText, buttonThreeText,
+                buttonFourText, buttonFiveText, buttonSixText,
+                buttonSevenText, buttonEightText, buttonNineText, gameState, new List<Button>() { bt1, bt2, bt3, bt4, bt5, bt6, bt7, bt8, bt9 });
+            GameControl.CheckForTie(AllButtonValues(), gameState, AllButtons());
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            GameControl.PlaceMove(buttonFiveText);
+            GameControl.GameStateUpdate(gameState);
+            GameControl.ValidateWinnerX(
+                buttonOneText, buttonTwoText, buttonThreeText,
+                buttonFourText, buttonFiveText, buttonSixText,
+                buttonSevenText, buttonEightText, buttonNineText, gameState, new List<Button>() { bt1, bt2, bt3, bt4, bt5, bt6, bt7, bt8, bt9 });
+            GameControl.ValidateWinnerO(
+                buttonOneText, buttonTwoText, buttonThreeText,
+                buttonFourText, buttonFiveText, buttonSixText,
+                buttonSevenText, buttonEightText, buttonNineText, gameState, new List<Button>() { bt1, bt2, bt3, bt4, bt5, bt6, bt7, bt8, bt9 });
+            GameControl.CheckForTie(AllButtonValues(), gameState, AllButtons());
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            GameControl.PlaceMove(buttonSixText);
+            GameControl.GameStateUpdate(gameState);
+            GameControl.ValidateWinnerX(
+                buttonOneText, buttonTwoText, buttonThreeText,
+                buttonFourText, buttonFiveText, buttonSixText,
+                buttonSevenText, buttonEightText, buttonNineText, gameState, new List<Button>() { bt1, bt2, bt3, bt4, bt5, bt6, bt7, bt8, bt9 });
+            GameControl.ValidateWinnerO(
+                buttonOneText, buttonTwoText, buttonThreeText,
+                buttonFourText, buttonFiveText, buttonSixText,
+                buttonSevenText, buttonEightText, buttonNineText, gameState, new List<Button>() { bt1, bt2, bt3, bt4, bt5, bt6, bt7, bt8, bt9 });
+            GameControl.CheckForTie(AllButtonValues(), gameState, AllButtons());
+        }
+
+        private void Button_Click_6(object sender, RoutedEventArgs e)
+        {
+            GameControl.PlaceMove(buttonSevenText);
+            GameControl.GameStateUpdate(gameState);
+            GameControl.ValidateWinnerX(
+                buttonOneText, buttonTwoText, buttonThreeText,
+                buttonFourText, buttonFiveText, buttonSixText,
+                buttonSevenText, buttonEightText, buttonNineText, gameState, new List<Button>() { bt1, bt2, bt3, bt4, bt5, bt6, bt7, bt8, bt9 });
+            GameControl.ValidateWinnerO(
+                buttonOneText, buttonTwoText, buttonThreeText,
+                buttonFourText, buttonFiveText, buttonSixText,
+                buttonSevenText, buttonEightText, buttonNineText, gameState, new List<Button>() { bt1, bt2, bt3, bt4, bt5, bt6, bt7, bt8, bt9 });
+            GameControl.CheckForTie(AllButtonValues(), gameState, AllButtons());
+        }
+
+        private void Button_Click_7(object sender, RoutedEventArgs e)
+        {
+            GameControl.PlaceMove(buttonEightText);
+            GameControl.GameStateUpdate(gameState);
+            GameControl.ValidateWinnerX(
+                buttonOneText, buttonTwoText, buttonThreeText,
+                buttonFourText, buttonFiveText, buttonSixText,
+                buttonSevenText, buttonEightText, buttonNineText, gameState, new List<Button>() { bt1, bt2, bt3, bt4, bt5, bt6, bt7, bt8, bt9 });
+            GameControl.ValidateWinnerO(
+                buttonOneText, buttonTwoText, buttonThreeText,
+                buttonFourText, buttonFiveText, buttonSixText,
+                buttonSevenText, buttonEightText, buttonNineText, gameState, new List<Button>() { bt1, bt2, bt3, bt4, bt5, bt6, bt7, bt8, bt9 });
+            GameControl.CheckForTie(AllButtonValues(), gameState, AllButtons());
+        }
+
+        private void Button_Click_8(object sender, RoutedEventArgs e)
+        {
+            GameControl.PlaceMove(buttonNineText);
+            GameControl.GameStateUpdate(gameState);
+            GameControl.ValidateWinnerX(
+                buttonOneText, buttonTwoText, buttonThreeText,
+                buttonFourText, buttonFiveText, buttonSixText,
+                buttonSevenText, buttonEightText, buttonNineText, gameState, new List<Button>() { bt1, bt2, bt3, bt4, bt5, bt6, bt7, bt8, bt9 });
+            GameControl.ValidateWinnerO(
+                buttonOneText, buttonTwoText, buttonThreeText,
+                buttonFourText, buttonFiveText, buttonSixText,
+                buttonSevenText, buttonEightText, buttonNineText, gameState, new List<Button>() { bt1, bt2, bt3, bt4, bt5, bt6, bt7, bt8, bt9 });
+            GameControl.CheckForTie(AllButtonValues(), gameState, AllButtons());
+        }
+
+        private List<Button> AllButtons()
+        {
+            List<Button> buttons = new List<Button>();
+            buttons.Add(bt1);
+            buttons.Add(bt2);
+            buttons.Add(bt3);
+            buttons.Add(bt4);
+            buttons.Add(bt5);
+            buttons.Add(bt6);
+            buttons.Add(bt7);
+            buttons.Add(bt8);
+            buttons.Add(bt9);
+            return buttons;
+        }
+        private List<TextBlock> AllButtonValues()
+        {
+            List<TextBlock> values = new List<TextBlock>();
+            values.Add(buttonOneText);
+            values.Add(buttonTwoText);
+            values.Add(buttonThreeText);
+            values.Add(buttonFourText);
+            values.Add(buttonFiveText);
+            values.Add(buttonSixText);
+            values.Add(buttonSevenText);
+            values.Add(buttonEightText);
+            values.Add(buttonNineText);
+            return values;
+        }
+        private void restartGameButton_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (TextBlock tb in AllButtonValues())
+            {
+                tb.Text = "";
+            }
+            foreach (Button bt in AllButtons())
+            {
+                bt.IsEnabled = true;
+            }
+            gameState.Text = "X Plays";
+            GameControl.ChangePlayer("X");
+        }
+    }
+}
